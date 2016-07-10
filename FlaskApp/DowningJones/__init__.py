5676ad2febfa368d67ea2d3d4f9cae2d328c3e6d
Nathan Adams <nathan.riley.adams@gmail.com>
2016-07-10 17:54:07 -0500

    Make collection pages dynamic

--- FlaskApp/DowningJones/__init__.py
+++ FlaskApp/DowningJones/__init__.py
@@ -3,23 +3,48 @@ app = Flask ( __name__ )
 
 @app.route ( '/' )
 def homepage ():
-	return render_template('index.html')
+	return render_template('index.html',
+							title='Downing Jones')
 
 @app.route ( '/index.html' )
 def index ():
-	return render_template('index.html')
+	return render_template('index.html',
+						    title='Downing Jones')
 
 @app.route ( '/about.html' )
 def about ():
 	return render_template('about.html')
 
-@app.route ( '/base.html' )
-def base ():
-	return render_template('base.html')
-
 @app.route ( '/companies.html' )
 def companies ():
-	return render_template('companies.html')
+	companies = [    
+		{
+            "id": 1,
+            "name": "Yahoo",
+            "symbol": "YHOO",
+            "stock_exchange": "NASDAQ",
+            "location": "USA",
+            "currency": "US Dollars"
+        },
+        {
+            "id": 2,
+            "name": "XILAM Animation",
+            "symbol": "XIL.PA",
+            "stock_exchange": "PAR",
+            "location": "France",
+            "currency": "Euro"
+        },
+        {
+            "id": 3,
+            "name": "The Vitec Group",
+            "symbol": "VTC.L",
+            "stock_exchange": "LSE",
+            "location": "Great Britain",
+            "currency": "Pound sterling"
+        }]
+	return render_template('companies.html',
+							title='Companies',
+							companies=companies)
 
 @app.route ( '/company1.html' )
 def company1 ():
@@ -35,7 +60,34 @@ def company3 ():
 
 @app.route ( '/currencies.html' )
 def currencies ():
-	return render_template('currencies.html')
+	currencies = [
+	    {
+            "id": 1,
+            "name": "US Dollars",
+            "currency_code": "USD",
+            "exchange": "NASDAQ",
+            "location": "USA",
+            "number_of_official_users": 9
+        },
+        {
+            "id": 2,
+            "name": "Euro",
+            "currency_code": "EUR",
+            "exchange": "PAR",
+            "location": "France",
+            "number_of_official_users": 19
+        },            
+        {
+            "id": 3,
+            "name": "Pound sterling",
+            "currency_code": "GBP",
+            "exchange": "LSE",
+            "location": "Great Britain",
+            "number_of_official_users": 1
+        }]
+	return render_template('currencies.html',
+							title='Currencies',
+							currencies=currencies)
 
 @app.route ( '/dollars.html' )
 def dollars ():
@@ -59,7 +111,35 @@ def greatbritain ():
 
 @app.route ( '/locations.html' )
 def locations ():
-	return render_template('locations.html')
+	locations = [
+	    {
+            "id": 1,
+            "location": "USA",
+            "capital": "Washington, D.C.",
+            "stock_exchange": "NASDAQ",
+            "currency": "US Dollars",
+            "GDP_Nominal": "$18.558 trillion" 
+
+        },
+        {
+            "id": 2,
+            "location": "France",
+            "capital": "Paris",
+            "stock_exchange": "PAR",
+            "currency": "Euro",
+            "GDP_Nominal": "$2.647 trillion"
+        },
+        {
+            "id": 3,
+            "location": "United Kingdom",
+            "capital": "London",
+            "stock_exchange": "LSE",
+            "currency": "Pound sterling",
+            "GDP_Nominal": "$2.849 trillion"
+        }]
+	return render_template('locations.html',
+						    title='Locations',
+						    locations=locations)
 
 @app.route ( '/lse.html' )
 def lse ():
@@ -75,7 +155,34 @@ def pounds ():
 
 @app.route ( '/stockmarkets.html' )
 def stockmarkets ():
-	return render_template('stockmarkets.html')
+	markets=[
+	        {
+                "id": 1,
+                "name": "NASDAQ",
+                "location": "USA",
+                "currency": "US Dollars",
+                "points": 4862.57,
+                "change": "+.041%"
+            },
+            {
+                "id": 2,
+                "name": "PAR",
+                "location": "France",
+                "currency": "Euro",
+                "points": 32.15,
+                "change": "-2.46%"
+            },
+            {
+                "id": 3,
+                "name": "LSE",
+                "location": "Great Britain",
+                "currency": "Pound sterling",
+                "points": 32.15,
+                "change": "-2.46%"
+            }]
+	return render_template('stockmarkets.html',
+							title='Exchanges',
+							markets=markets)
 
 @app.route ( '/us.html' )
 def us ():
