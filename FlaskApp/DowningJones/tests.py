52ec4967a83a223337b986951fa41397e0cc2091
Sarah Jee <sjee96@cs.utexas.edu>
2016-07-12 21:27:14 -0500

    more work on units again

--- FlaskApp/DowningJones/tests.py
+++ FlaskApp/DowningJones/tests.py
@@ -75,7 +75,7 @@ class MyTest(TestCase):
         db.session.commit()
 
         #assert it returns correct data
-        self.assertEqual('\'USD\'', str(location.query.first().currency))
+        self.assertEqual('USD', str(location.query.first().currency))
 
         self.assertEqual('<Exchange \'NMS\'>', str(location.query.first().exchanges.first()))
 
@@ -137,7 +137,7 @@ class MyTest(TestCase):
         db.session.commit()
 
         #assert it retuns correct data
-        self.assertEqual('<Location \'USA\'>', str(company.query.first().location))
+        self.assertEqual('USA', str(company.query.first().location))
 
         self.assertEqual('<Exchange \'NMS\'>', str(company.query.first().exchange))
 
