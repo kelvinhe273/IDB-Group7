f9a8af4aedc2e32ebeaf5e90eb54462233a3e8e9
almto3 <almto3@hotmail.com>
2016-07-14 16:28:59 -0500

    added js fuctions on the currncies page

--- FlaskApp/DowningJones/static/count.js
+++ FlaskApp/DowningJones/static/count.js
@@ -4,23 +4,52 @@ function count_countries(str){
 	var ref = ["Canada", "Great Britain", "Greece", "United States", "Mexico", "Germany", "Taiwan", "Germany", "Germany", "Hong Kong", "France"];
 	
 	if(str.includes(",")){
-		
 		var tmp = str.split(",");
 		for(i = 0; i < tmp.length; i++)
 			tmp[i] = ref.indexOf(tmp[i]) + 1;
 
-		console.log("if- " + tmp);
+		//console.log("if: " + tmp);
 
-		console.log("link: " + "/locations/" + tmp[0]);
-		console.log("id: " + "/locations/" + id);
+		//console.log("link: " + "/locations/" + tmp[0]);
+		//console.log("id: " + "/locations/" + id);
 		document.getElementById(id).href = "/locations/" + tmp[0];
 	}
 	else{
-		console.log("else: " + str);
+		//console.log("else: " + str);
 		str = ref.indexOf(str) + 1;
-		console.log("link: " + "/locations/" + str);
-		console.log("id: " + "/locations/" + id);
+		//console.log("link: " + "/locations/" + str);
+		//console.log("id: " + id);
 		document.getElementById(id).href = "/locations/" + str;
-		return true;
 	}
+	return true;
+}
+
+function count_countries_helper(){
+	console.log("GJGJGJG");
+	countries = document.getElementsByClassName("country");
+	var repeated = [];
+
+	for(i = 0; i< countries.length; i++){
+		console.log(i + ": " + trim(countries.item(i).textContent) + " " + countries.length);
+
+		//countries.item(i).innerHTML = trim(countries.item(i).textContent);
+
+		if (repeated.indexOf(countries.item(i).textContent) == -1){
+			console.log(countries.item(i).textContent + " NOT REPEATED")
+			count_countries(trim(countries.item(i).textContent));
+			repeated.push(countries.item(i).textContent);
+		}
+		else{
+			console.log(countries.item(i).textContent + " REPEATED")
+		}
+	}
+	
+}
+
+function _print(){
+	console.log("called print");
+}
+
+function trim(s){ 
+ return ( s || '' ).replace( /^\s+|\s+$/g, '' ); 
 }
